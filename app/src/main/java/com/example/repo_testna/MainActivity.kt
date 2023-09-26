f44180206c7fabe60e27320ab477558a2463db6b
lhmuana <edd_vai@yahoo.co.in>
2023-09-26 16:25:07 +0530

    Make a box composable to cover all the elements and make it eleveated

--- app/src/main/java/com/example/repo_testna/MainActivity.kt
+++ app/src/main/java/com/example/repo_testna/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -56,14 +58,35 @@ data class ImageInfo(val resourceId: Int, val description: String, val author: S
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
 
-    val images = listOf(
-        ImageInfo(resourceId = R.drawable.london_bridge,description ="London Bridge", author="Frank Shamrock (2021)" ),
-        ImageInfo(resourceId = R.drawable.eiffel_tower,description ="Eiffel Tower", author="Royce Gracie (2019)"  ),
-        ImageInfo(resourceId = R.drawable.statue_of_liberty,description ="Statue of liberty", author="Tim Dillion (2020)")
-    )
-        Column(modifier = Modifier.padding(top= 20.dp),
-                          verticalArrangement = Arrangement.Center,
-                          horizontalAlignment = Alignment.CenterHorizontally  ) {
+    Box( modifier = Modifier
+        .size(200.dp)
+        .clip(RoundedCornerShape(8.dp))
+        .background(Color.White)
+        .padding(16.dp) // Add padding to create the elevated effect
+        .shadow(4.dp, shape = RoundedCornerShape(8.dp))) // Add shadow) {
+
+        val images = listOf(
+            ImageInfo(
+                resourceId = R.drawable.london_bridge,
+                description = "London Bridge",
+                author = "Frank Shamrock (2021)"
+            ),
+            ImageInfo(
+                resourceId = R.drawable.eiffel_tower,
+                description = "Eiffel Tower",
+                author = "Royce Gracie (2019)"
+            ),
+            ImageInfo(
+                resourceId = R.drawable.statue_of_liberty,
+                description = "Statue of liberty",
+                author = "Tim Dillion (2020)"
+            )
+        )
+        Column(
+            modifier = Modifier.padding(top = 20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             val image = painterResource(R.drawable.london_bridge)
 
             var currentIndex by remember { mutableStateOf(0) }
@@ -71,7 +94,7 @@ fun Greeting(modifier: Modifier = Modifier) {
             Image(
                 painter = painterResource(id = images[currentIndex].resourceId),
                 contentDescription = images[currentIndex].description,
-                modifier = Modifier.size(height=180.dp, width= 180.dp)
+                modifier = Modifier.size(height = 200.dp, width = 200.dp)
 
             )
 
@@ -86,33 +109,38 @@ fun Greeting(modifier: Modifier = Modifier) {
                 fontSize = 10.sp
             )
             Spacer(Modifier.height(20.dp))
-            Row(){
-                    Button(onClick = { if (currentIndex > 0) {
-                        currentIndex--
-                    }
+            Row() {
+                Button(
+                    onClick = {
+                        if (currentIndex > 0) {
+                            currentIndex--
+                        }
                     },
-                        enabled = currentIndex > 0
-                    )
-                    {
-                        Text("Previous")
-                    }
+                    enabled = currentIndex > 0
+                )
+                {
+                    Text("Previous")
+                }
 
 
-                    Button(onClick = { if (currentIndex < images.size - 1) {
-                        currentIndex++
-                    }
+                Button(
+                    onClick = {
+                        if (currentIndex < images.size - 1) {
+                            currentIndex++
+                        }
                     },
-                        enabled = currentIndex < images.size - 1,
-                        modifier = Modifier.padding(start = 20.dp)
-                    )
+                    enabled = currentIndex < images.size - 1,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
 
-                         {
-                        Text("Next")
-                    }
+                {
+                    Text("Next")
+                }
             }
         }
 
-}
+    }
+
 
 @Preview(showBackground = true)
 @Composable
