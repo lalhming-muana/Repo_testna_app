773067748d4949a1713c5fbe8727b6e8f834ede6
lhmuana <edd_vai@yahoo.co.in>
2023-09-26 12:50:47 +0530

    Basic layout complete

--- app/src/main/java/com/example/repo_testna/MainActivity.kt
+++ app/src/main/java/com/example/repo_testna/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.repo_testna
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.repo_testna.ui.theme.Repo_testnaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +40,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Box(modifier = Modifier.fillMaxSize()){
+        Column(modifier = Modifier.padding(top= 20.dp),
+                          verticalArrangement = Arrangement.Center,
+                          horizontalAlignment = Alignment.CenterHorizontally  ) {
+            val image = painterResource(R.drawable.london_bridge)
+
+            Image(
+                painter = image,
+                contentDescription = null,
+
+            )
+
+            Text(
+                text = "London Bridge London",
+                modifier = modifier
+            )
+
+            Row(){
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("Previous")
+                    }
+
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("Next")
+                    }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Repo_testnaTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
